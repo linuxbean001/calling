6647c38598d29036e11c309882955cc36669e222
Vishnu linux <vishnulinux02@gmail.com>
2021-03-12 14:57:13 +0530

    data

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ const callback = () => {
     'content-type': 'application/x-www-form-urlencoded',
 	'Authorization' : 'Basic eW1INnJKR3RmWE42bGZYVDp3SGU1Y0VwT2pGVVVUM1ZyektBVU9vYlVWdmtJU2prQQ==',
 	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+	//'Access-Control-Max-Age': '7200',
+	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept,Authorization',
+	'Access-Control-Allow-Credentials': true
 };
 
 const dataString = {grant_type:"client_credentials"};
